62da65868165e5e0d058d35088040ffd25a3aa9d
wmpscc <1214490813@qq.com>
2019-02-27 21:54:50 +0800

    :fire:update

--- AlexNet/CreateTFRecord.py
+++ AlexNet/CreateTFRecord.py
@@ -4,7 +4,7 @@ import tensorflow as tf
 import random
 import os
 from tqdm import tqdm
-import cv2
+import threading
 
 trainPaths = list()
 testPaths = list()
@@ -51,30 +51,43 @@ def probuf(label, image_raw):
     return example.SerializeToString()
 
 
-def writerRecord(save_path, HOME_PATH):
-    writerTrain = tf.python_io.TFRecordWriter(os.path.join(save_path, "train.tfrecords"))
-    writerTest = tf.python_io.TFRecordWriter(os.path.join(save_path, "test.tfrecords"))
+def writerRecord(save_path, HOME_PATH, coord, t_id):
+    writerTrain = tf.python_io.TFRecordWriter(os.path.join(save_path, "train" + str(t_id) + ".tfrecords"))
+    writerTest = tf.python_io.TFRecordWriter(os.path.join(save_path, "test" + str(t_id) + ".tfrecords"))
     randIndexTrain = random.sample(range(0, len(trainPaths)), len(trainPaths))
-    randIndexTest = random.sample(range(0, len(testPaths)), len(testPaths))
 
-    for i in tqdm(randIndexTrain, "Start write train tfrecords"):
+    size = int(len(randIndexTrain) / 4)
+    randIndexTrain = randIndexTrain[t_id * size: (t_id + 1) * size]
+
+    randIndexTest = random.sample(range(0, len(testPaths)), len(testPaths))
+    size = int(len(randIndexTest) / 4)
+    randIndexTest = randIndexTest[t_id * size: (t_id + 1) * size]
+
+    sess = tf.Session()
+    for i in tqdm(randIndexTrain, "thread" + str(t_id) + "Start write train tfrecords"):
         image_string = tf.read_file(os.path.join(HOME_PATH, trainPaths[i] + ".jpg"))
         image_string = sess.run(image_string)
         label = trainPaths[i].split("/")[0]
         label = transform_label(label)
         writerTrain.write(probuf(label, image_string))
     writerTrain.close()
-    for i in tqdm(randIndexTest, "Start write test tfrecords"):
+    for i in tqdm(randIndexTest, "thread" + str(t_id) + "Start write test tfrecords"):
         image_string = tf.read_file(os.path.join(HOME_PATH, testPaths[i] + ".jpg"))
         image_string = sess.run(image_string)
         label = testPaths[i].split("/")[0]
         label = transform_label(label)
         writerTest.write(probuf(label, image_string))
     writerTest.close()
+    coord.request_stop()
 
 
 if __name__ == '__main__':
-    sess = tf.InteractiveSession()
+    # sess = tf.InteractiveSession()
+    coord = tf.train.Coordinator()
     load_files()
-    writerRecord("/media/data/oldcopy/PythonProject/Food101/TFRecord",
-                 "/media/data/oldcopy/PythonProject/Food101/food-101/images")
+    threads = [threading.Thread(target=writerRecord, args=("/media/data/oldcopy/PythonProject/Food101/TFRecord",
+                                                           "/media/data/oldcopy/PythonProject/Food101/food-101/images",
+                                                           coord, i)) for i in range(4)]
+    for t in threads:
+        t.start()
+    coord.join(threads)
